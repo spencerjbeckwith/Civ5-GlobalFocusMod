697cfff6983fea7924c4a8b390db39d0c5028128
Spencer Beckwith <spencerjbeckwith@gmail.com>
2024-08-22 18:25:24 -0700

    Menu layout

--- src/GlobalFocus.lua
+++ src/GlobalFocus.lua
@@ -1,1 +1,27 @@
---
+ContextPtr:SetHide(true);
+
+-- Add menu to additional information dropdown
+LuaEvents.AdditionalInformationDropdownGatherEntries.Add(
+    function(entries)
+        table.insert(entries, {
+            text = Locale.ConvertTextKey("TXT_KEY_GLOBALFOCUS_TITLE"),
+            call = function()
+                ContextPtr:SetHide(false);
+            end,
+        });
+    end
+);
+LuaEvents.RequestRefreshAdditionalInformationDropdownEntries();
+
+-- Let the menu be closed with the escape key
+ContextPtr:SetInputHandler(
+    function(uiMsg, wParam, lParam)
+        if (uiMsg == KeyEvents.KeyDown) then
+            if (wParam == Keys.VK_ESCAPE) then
+                ContextPtr:SetHide(true);
+                return true;
+            end
+        end
+        return false;
+    end
+);
